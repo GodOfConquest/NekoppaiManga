ccaa5de1cad5ae945716fdbedc3256948f295296
Aftershock <GodOfConquest@users.noreply.github.com>
2015-06-02 23:59:25 +0530

    Update info.php

--- app/themes/default/info.php
+++ app/themes/default/info.php
@@ -4,9 +4,9 @@
 			"name" => "Default theme",
 			"description" => "This is default theme",
 			"version" => '1.01',
-			"author" => "Huy Trinh",
-			"authorEmail" => "ihnep11@gmail.com",
-			"authorURL" => "http://huykhong.com",
+			"author" => "Arijit Dey",
+			"authorEmail" => "idiosyncrasys@gmail.com",
+			"authorURL" => "http://wikichan.com",
 			"license" => "GPLv2"	
 	);
 
